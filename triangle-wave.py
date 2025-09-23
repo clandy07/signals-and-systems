56fb59e84456bd2ad17c614f8e911265f9ce07a5
clandy07 <127475702+clandy07@users.noreply.github.com>
2025-09-23 14:36:05 +0800

    feat: update triangle-wave.py

--- triangle-wave.py
+++ triangle-wave.py
@@ -8,8 +8,9 @@ num_harmonics = 100
 
 for i in range(num_harmonics):
     k = 2 * i + 1 # k = 1, 3, ,5...
-    y += ((-1)**i) * (1 / k**2) * np.sin(k * t)
+    y += (-4 / (k * np.pi)**2) * np.cos(k * t)
 
 plt.plot(t, y)
 plt.title(f'Triangle Wave Synthesis ({num_harmonics} terms)')
+
 plt.show()
